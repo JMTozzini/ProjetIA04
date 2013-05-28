0c9970a58f6a3d027b0e48bbab4c1ddbad614af0
Jean-Michel Tozzini <jeanmichel53@hotmail.com>
2013-05-28 15:18:51 +0200

    Implementation génération de route

--- src/com/ia04/constantes/ConstantesEnv.java
+++ src/com/ia04/constantes/ConstantesEnv.java
@@ -3,10 +3,10 @@ package com.ia04.constantes;
 public class ConstantesEnv {
 	public static double PROP_VEG_FAIBLE	= 0.20;
 	public static double PROP_VEG_MOY		= 0.20;
-	public static double PROP_VEG_FORTE		= 0.15;
+	public static double PROP_VEG_FORTE		= 0.25;
 	public static double PROP_HABITATION	= 0.15;
 	public static double PROP_ROCHE			= 0.05;
 	public static double PROP_EAU			= 0.10;
-	public static double PROP_ROUTE			= 0.15;
+	public static double PROP_ROUTE			= 0.05;
 	// ce sont des proportions donc la somme est 1 !
 }
--- src/com/ia04/constantes/ConstantesGenerales.java
+++ src/com/ia04/constantes/ConstantesGenerales.java
@@ -2,5 +2,5 @@ package com.ia04.constantes;
 
 public class ConstantesGenerales {
 	public static int FRAME_SIZE 	= 400;
-	public static int GRID_SIZE 	= 75;
+	public static int GRID_SIZE 	= 50;
 }
